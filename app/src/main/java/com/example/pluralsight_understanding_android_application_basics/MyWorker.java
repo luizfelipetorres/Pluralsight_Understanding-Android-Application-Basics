a72d6c9deb0a49f2db36c95ab4dba9448229b573
luizfelipetorres <fellipe116@gmail.com>
2022-08-31 10:58:51 -0300

    Module 1 completed

---
+++ app/src/main/java/com/example/pluralsight_understanding_android_application_basics/MyWorker.java
@@ -0,0 +1,7 @@
+package com.example.pluralsight_understanding_android_application_basics;
+
+public class MyWorker {
+    public static int doubleTheValue(int value){
+        return value * 2;
+    }
+}
